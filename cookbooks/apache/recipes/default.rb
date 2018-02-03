571d3e2d853948d8e0856ef95024f91ed20b3ad8
Nakamura Kosuke <ncl0709@gmail.com>
2018-02-03 14:18:14 +0900

    to template http.conf

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -5,3 +5,11 @@ end
 service "httpd" do
   action %w(enable start)
 end
+
+template "httpd.conf" do
+  path "/etc/httpd/conf/httpd.conf"
+  owner "root"
+  group "root"
+  mode 0644
+  notifies :reload, 'service[httpd]'
+end
