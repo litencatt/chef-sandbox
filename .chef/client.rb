b02e0db8319dd668f11760ed44ee2e80f8b578dc
Nakamura Kosuke <ncl0709@gmail.com>
2018-04-08 01:21:01 +0900

    fix

--- .chef/client.rb
+++ .chef/client.rb
@@ -1,4 +1,4 @@
-current_dir = '/opt/muumuu-chef'
+current_dir = '/opt/chef'
 cookbook_path ["#{current_dir}/cookbooks", "#{current_dir}/vendor/cookbooks"]
 role_path "#{current_dir}/roles"
 environment_path "#{current_dir}/environments"
