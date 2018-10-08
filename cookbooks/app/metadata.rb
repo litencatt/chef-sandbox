9db6328480b8d3526346cb3a29272c61887138c6
Nakamura Kosuke <ncl0709@gmail.com>
2018-10-08 17:58:49 +0900

    Add cookbooks for app

---
+++ cookbooks/app/metadata.rb
@@ -0,0 +1,8 @@
+name             'proxy'
+license          'All rights reserved'
+description      'Installs/Configures base'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends 'motd'
+depends 'apache'
---
+++ cookbooks/app/recipes/default.rb
@@ -0,0 +1,4 @@
+# Cookbook Name:: base
+# Recipe:: default
+
+include_recipe 'apache'
