9280070837da8ef8ac5f786b4202a5d316b30a27
Nakamura Kosuke <ncl0709@gmail.com>
2018-10-08 19:55:45 +0900

    Add cookbooks/php

--- cookbooks/app/recipes/default.rb
+++ cookbooks/app/recipes/default.rb
@@ -2,3 +2,4 @@
 # Recipe:: default
 
 include_recipe 'apache'
+include_recipe 'php'
--- cookbooks/php/metadata.rb
+++ cookbooks/php/metadata.rb
@@ -1,9 +1,5 @@
-name             'proxy'
+name             'php'
 license          'All rights reserved'
-description      'Installs/Configures base'
+description      'Install php5.6'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
-
-depends 'motd'
-depends 'apache'
-depends 'php'
