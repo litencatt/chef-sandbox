fa2565f0d7a33421acc30ef1cdb43ff7d8711665
Nakamura Kosuke <ncl0709@gmail.com>
2019-01-19 17:29:07 +0900

    Delete

--- cookbooks/php/metadata.rb
+++
@@ -1,5 +0,0 @@
-name             'php'
-license          'All rights reserved'
-description      'Install php5.6'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
