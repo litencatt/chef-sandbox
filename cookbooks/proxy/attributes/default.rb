9f5a4206ab6c8494a9a1fdbf7d7b926d72802c3d
Nakamura Kosuke <ncl0709@gmail.com>
2019-06-08 11:13:15 +0900

    Use own nginx.conf temlate

--- cookbooks/proxy/attributes/default.rb
+++ cookbooks/proxy/attributes/default.rb
@@ -1,2 +1,4 @@
-default['nginxxx']['version']                  = '1.17.0'
-default['nginxxx']['build']                    = true
+default['nginxxx']['version']       = '1.17.0'
+default['nginxxx']['build']         = true
+default['nginxxx']['default_site']  = false
+default['nginxxx']['conf_cookbook'] = 'proxy'
