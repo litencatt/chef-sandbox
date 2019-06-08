4576771fb5aaabf87291abfdf02c294cd2634d98
Nakamura Kosuke <ncl0709@gmail.com>
2019-06-08 12:39:33 +0900

    Use own server conf file

--- cookbooks/proxy/attributes/default.rb
+++ cookbooks/proxy/attributes/default.rb
@@ -1,4 +1,4 @@
 default['nginxxx']['version']       = '1.17.0'
 default['nginxxx']['build']         = true
-default['nginxxx']['default_site']  = true
+default['nginxxx']['default_site']  = false
 default['nginxxx']['conf_cookbook'] = 'proxy'
